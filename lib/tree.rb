d1c9a7077b74f65b07d8f9e3bcf8930c0a040fa8
BlueVajra <cory.leistikow@gmail.com>
2014-07-01 09:22:31 -0600

    user can move through tree printing or adding names to array

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,28 @@
 require 'node'
 
 class Tree
+  def initialize
+    @result_array = []
+  end
 
+  def print_names(node)
+    puts node.name
+    node.children.each do |child_node|
+      print_names(child_node)
+    end
+  end
+
+  def names(node)
+    add_names(node)
+    @result_array
+  end
+
+  def add_names(node)
+    @result_array << node.name
+    node.children.each do |child_node|
+      add_names(child_node)
+    end
+  end
 
 
 end
